19d97432a8646d665ffc623e23343efee53fd2db
libbies <bcw@well.com>
2020-12-04 09:55:51 -0500

    rewrites for the day 4 solutions

--- day4/part2.py
+++ day4/part2.py
@@ -8,7 +8,7 @@ print(len([
     if 1920 <= int(p["byr"]) <= 2002
     if 2010 <= int(p["iyr"]) <= 2020
     if 2020 <= int(p["eyr"]) <= 2030
-    if (p["hgt"].endswith("cm") or p["hgt"].endswith("in"))
+    if p["hgt"].endswith("cm") or p["hgt"].endswith("in")
     if not(p["hgt"].endswith("cm") and not 150 <= int(p["hgt"][:-2]) <= 193)
     if not(p["hgt"].endswith("in") and not 59 <= int(p["hgt"][:-2]) <= 76)
     if len(p["hcl"])==7
