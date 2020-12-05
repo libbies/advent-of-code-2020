4b855b8228916988d9baacb082534bfbfe615b31
libbies <bcw@well.com>
2020-12-05 13:21:03 -0500

    made the one-liners shorter...

--- day5/day5-one-liners.py
+++ day5/day5-one-liners.py
@@ -1,5 +1,5 @@
 # part 1
-print("part1:", max([int(str.translate(p,str.maketrans('FBLR','0101')),2) for p in [l.strip() for l in open("input.txt").readlines()]]))
+print("part1:", max([int(str.translate(p,str.maketrans('FBLR','0101')),2) for p in open("input.txt").read().splitlines()]))
 
 # part 2
-print("part2:", [[*set(range(min(s), max(s)))-set(s)][0] for s in [[int(str.translate(p,str.maketrans('FBLR','0101')),2) for p in [l.strip() for l in open("input.txt").readlines()]]]][0])
+print("part2:", [[*set(range(min(s), max(s)))-set(s)][0] for s in [[int(str.translate(p,str.maketrans('FBLR','0101')),2) for p in open("input.txt").read().splitlines()]]][0])
