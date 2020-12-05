df50013b7d5182a30a6aeabd4539780f48186cc4
libbies <bcw@well.com>
2020-12-05 01:08:53 -0500

    day 5 solutions

---
+++ day5/part1.py
@@ -0,0 +1,19 @@
+passes = [l.strip() for l in open("input.txt").readlines()]
+
+seats = dict()
+for p in passes:
+    row_min, row, row_max = 0, 64, 128
+    for s in [c for c in p][:-3]:
+        if s=='F':
+            row_max, row = row, (row_min+row)//2
+        elif s=='B':
+            row_min, row = row, (row_max+row)//2
+    col_min, col, col_max = 0, 4, 8
+    for s in [c for c in p][7:]:
+        if s=='L':
+            col_max, col = col, (col_min+col)//2
+        elif s=='R':
+            col_min, col = col, (col_max+col)//2
+    seats[p] = row*8+col
+
+print(sorted(seats.items(), key=lambda x: x[1])[-5:])
