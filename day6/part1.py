53030ba55820120c0e307a086b2d17600ae1dd0f
libbies <bcw@well.com>
2020-12-06 10:59:03 -0500

    removed unnecessary reduce()

--- day6/part1.py
+++ day6/part1.py
@@ -1,4 +1,4 @@
-from functools import reduce
+# from functools import reduce
 # from operator import or_
 
 groups = map(str.split, open("input.txt").read().split('\n\n'))
@@ -6,9 +6,9 @@ groups = map(str.split, open("input.txt").read().split('\n\n'))
 total = 0
 for group in groups:
     # total += len(reduce(or_, [set(s) for s in group]))
-    total += len(reduce(set.union, (set(g) for g in group)))
+    total += len(set.union(*[set(g) for g in group]))
 
 print(total)
 
 # as a one-liner
-# print("part1:", sum([len(__import__("functools").reduce(set.union, (set(g) for g in group))) for group in map(str.split, open("input.txt").read().split('\n\n'))]))
+# print("part1:", sum([len(set.union(*[set(g) for g in group])) for group in map(str.split, open("input.txt").read().split('\n\n'))]))
--- day6/part2.py
+++ day6/part2.py
@@ -1,14 +1,14 @@
-from functools import reduce
+# from functools import reduce
 # from operator import or_
 
 groups = map(str.split, open("input.txt").read().split('\n\n'))
 
 total = 0
 for group in groups:
-    # total += len(reduce(and_, [set(s) for s in group]))
-    total += len(reduce(set.intersection, (set(g) for g in group)))
+    # total += len(reduce(or_, [set(s) for s in group]))
+    total += len(set.intersection(*[set(g) for g in group]))
 
 print(total)
 
-# as one-liner
-# print("part2:", sum([len(__import__("functools").reduce(set.intersection, (set(g) for g in group))) for group in map(str.split, open("input.txt").read().split('\n\n'))]))
+# as a one-liner
+# print("part2:", sum([len(set.intersection(*[set(g) for g in group])) for group in map(str.split, open("input.txt").read().split('\n\n'))]))
