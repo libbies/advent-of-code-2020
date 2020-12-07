dce44df7144de0f89b8ccff8ea4a6d313eb22813
libbies <bcw@well.com>
2020-12-07 01:56:52 -0500

    tidying up

--- day7/part1.py
+++ day7/part1.py
@@ -11,9 +11,8 @@ queue = ["shiny gold"]
 while queue:
     for bag, contents in bags.items():
         if [c[1] for c in contents if c[1].startswith(queue[0])]:
-            # print(queue[0], bag, [c[1] for c in contents])
             outer[bag] = (queue[0], [c[1] for c in contents])
             queue.append(bag.rsplit(maxsplit=1)[0])
-    _ = queue.pop(0)
+    queue.pop(0)
 
-print(len(outer))
+print("part1:", len(outer))
--- day7/part2.py
+++ day7/part2.py
@@ -11,22 +11,18 @@ queue = ["shiny gold"]
 while queue:
     for bag, contents in bags.items():
         if bag.startswith(queue[0]):
-            # print(queue[0], bag, [c[1] for c in contents])
-            inner[bag.rsplit(maxsplit=1)[0]] = [(c[0], c[1].rsplit(maxsplit=1)[0]) for c in contents]
-            queue += [c[1].rsplit(maxsplit=1)[0] for c in contents if c[0]!="no"]
-    # print(queue)
-    _ = queue.pop(0)
+            inner[bag.rsplit(maxsplit=1)[0]] = [
+                    (c[0], c[1].rsplit(maxsplit=1)[0]) for c in contents
+                ]
+            queue += [c[1].rsplit(maxsplit=1)[0] for c in contents]
+    queue.pop(0)
 
 def search(bag):
-    global inner
-    if bag in inner:
-        if inner[bag]==[('no', 'other')]:
-            return(1)
-        s = 0
-        for (c,b) in inner[bag]:
-            # print(bag, c, b)
-            s += int(c) * search(b)
-        # print(bag, repr(inner[bag]), s)
-        return(1+s)
+    if bag not in inner or inner[bag]==[('no', 'other')]:
+        return(1)
+    s = 0
+    for (c, b) in inner[bag]:
+        s += int(c) * search(b)
+    return(1+s)
 
-print(search("shiny gold")-1)
+print("part2:", search("shiny gold")-1)
