8e7b3e7e263a49e76f555a98bdd89e57be3ba5d0
libbies <bcw@well.com>
2020-12-10 03:48:01 -0500

    tidying up

--- day10/part2b.py
+++ day10/part2b.py
@@ -3,5 +3,5 @@ from operator import mul
 inputs = sorted(map(int, open("input.txt").read().splitlines()))
 counts = ''.join([str(max(1, c-inputs[i-1])) for i, c in enumerate(inputs)]).split('3')
 # print("part1:", (len(counts)+1) * (len(''.join(counts))-2))
-permutations = [max(1, 2**(len(c)-1)) if len(c)<4 else (2**(len(c)-1))-1 for c in counts]
+permutations = [max(1, 2**(len(c)-1)) if len(c)<4 else 2**(len(c)-1)-1 for c in counts]
 print("part2:", reduce(mul, permutations))
