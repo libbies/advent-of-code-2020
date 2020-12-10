6cb1dce3fa3b66853bba5b20cbbfa7b894903335
libbies <bcw@well.com>
2020-12-10 03:38:26 -0500

    some adjustments to the math

--- day10/part2b.py
+++ day10/part2b.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from operator import mul
-inputs = [0] + sorted(map(int, open("input.txt").read().splitlines()))
-counts = ''.join([str(c-inputs[i-1]) for i, c in enumerate(inputs)][1:]).split('3')
-print("part2:", reduce(mul, [max(1, 2**(len(c)+1-2)) if len(c)+1 <= 4 else ((2**(len(c)+1-2))-1) for c in counts]))
+inputs = sorted(map(int, open("input.txt").read().splitlines()))
+counts = ''.join([str(max(1, c-inputs[i-1])) for i, c in enumerate(inputs)]).split('3')
+# print("part1:", (len(counts)+1) * (len(''.join(counts))-2))
+print("part2:", reduce(mul, [max(1, 2**(len(c)-1)) if len(c)+1 <= 4 else ((2**(len(c)-1))-1) for c in counts]))
