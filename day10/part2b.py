0dcdeac2c5a267bd9a1d3168e917ebe09f8c3de9
libbies <bcw@well.com>
2020-12-10 09:53:16 -0500

    refactoring?

--- day10/part2b.py
+++ day10/part2b.py
@@ -1,7 +1,18 @@
-from functools import reduce
-from operator import mul
 inputs = sorted(map(int, open("input.txt").read().splitlines()))
-counts = ''.join([str(max(1, c-inputs[i-1])) for i, c in enumerate(inputs)]).split('3')
-# print("part1:", (len(counts)+1) * (len(''.join(counts))-2))
-permutations = [max(1, 2**(len(c)-1)) if len(c)<4 else 2**(len(c)-1)-1 for c in counts]
-print("part2:", reduce(mul, permutations))
+
+diffs = str()
+for i, d in enumerate(inputs):
+    diffs += ''.join(str(max(1, d-inputs[i-1])))
+
+print("part1:", diffs.count('1') * (diffs.count('3')+1))
+
+diffs = [d for d in diffs.split('3') if d]
+
+total = 1
+for d in diffs:
+    if len(d) < 4:
+        total *= max(1, 2**(len(d)-1))
+    else:
+        total *= 2**(len(d)-1)-1
+
+print("part2:", total)
