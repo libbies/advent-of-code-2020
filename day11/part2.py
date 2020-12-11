542a30203a1175e501b37737a413258f50027393
libbies <bcw@well.com>
2020-12-11 08:12:05 -0500

    tidying up

--- day11/part2.py
+++ day11/part2.py
@@ -1,27 +1,24 @@
 # from pprint import pprint
-# from copy import deepcopy
 
 rows = [' '+r+' ' for r in open("input.txt").read().replace('.',' ').splitlines()]
 rows = [' '*len(rows[0])] + rows + [' '*len(rows[0])]
-# rows = {i: {j: c for j, c in enumerate(f' {r} ')} for i,r in enumerate(rows)}
 rows = [[c for c in r] for r in rows]
 
-lr, lc = len(rows), len(rows[0])
+lr, lc, l = len(rows), len(rows[0]), max(len(rows), len(rows[0]))
 iterations = 0
 while True:
     # print(iterations)
-    state = [r[:] for r in rows[:]]
-    # state = deepcopy(rows)
+    state = [r.copy() for r in rows]
     for x in range(1, len(rows)-1):
         for y in range(1, len(rows[0])-1):
-            up = (rows[x-n][y] for n in range(1, x+1) if rows[x-n][y]!=' ')
+            up = (rows[x-n][y] for n in range(1,  x+1) if rows[x-n][y]!=' ')
             dn = (rows[x+n][y] for n in range(1, lr-x) if rows[x+n][y]!=' ')
-            ll = (rows[x][y-n] for n in range(1, y+1) if rows[x][y-n]!=' ')
+            ll = (rows[x][y-n] for n in range(1,  y+1) if rows[x][y-n]!=' ')
             rr = (rows[x][y+n] for n in range(1, lc-y) if rows[x][y+n]!=' ')
-            ul = (rows[x-n][y-n] for n in range(1, min(x, y)) if 0<x-n if 0<y-n if rows[x-n][y-n]!=' ')
-            ur = (rows[x-n][y+n] for n in range(1, max(x, lc-y)) if 0<x-n if y+n<lc if rows[x-n][y+n]!=' ')
-            dl = (rows[x+n][y-n] for n in range(1, max(lr-x, y)) if 0<y-n if x+n<lr if rows[x+n][y-n]!=' ')
-            dr = (rows[x+n][y+n] for n in range(1, max(lr-x, lc-y)) if x+n<lr if y+n<lc if rows[x+n][y+n]!=' ')
+            ul = (rows[x-n][y-n] for n in range(1, l) if x-n > 0 if y-n > 0 if rows[x-n][y-n]!=' ')
+            ur = (rows[x-n][y+n] for n in range(1, l) if x-n > 0 if y+n <lc if rows[x-n][y+n]!=' ')
+            dl = (rows[x+n][y-n] for n in range(1, l) if x+n <lr if y-n > 0 if rows[x+n][y-n]!=' ')
+            dr = (rows[x+n][y+n] for n in range(1, l) if x+n <lr if y+n <lc if rows[x+n][y+n]!=' ')
             cells = [next(up, None), next(dn, None), next(ll, None), next(rr, None),
                      next(ul, None), next(ur, None), next(dl, None), next(dr, None)]
             if rows[x][y] == 'L' and '#' not in cells:
@@ -34,5 +31,4 @@ while True:
         rows = state
         iterations += 1
 
-# print(sum([[*r.values()].count('#') for r in rows.values()]))
 print(iterations, "iterations, part1:", ''.join([''.join([c for c in r if c=='#']) for r in rows]).count('#'))
