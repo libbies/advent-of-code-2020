2770b901f6eea45b9255bd5fa4ceaecbaa2f0301
libbies <bcw@well.com>
2020-12-25 15:41:47 -0500

    cleaning up

--- day13/part2.py
+++ day13/part2.py
@@ -1,15 +1,18 @@
 from functools import reduce
 from operator import mul
 
-input = open("input.txt").read().splitlines()
-
-buses = dict()
-for offset, bus in enumerate(int(b) if b.isdigit() else None for b in input[1].split(',')):
-    if bus:
-        buses[bus] = offset
-
 def crt(n, a):
     p = reduce(mul, n)
     return sum(ai * pow(p//ni, -1, ni) * (p//ni) for (ni, ai) in zip(n, a)) % p
+
+input = [
+    int(b) if b.isdigit() else None
+    for b in open("input.txt").read().splitlines()[-1].split(',')
+]
+
+buses = dict()
+for offset, bus in enumerate(input):
+    if bus:
+        buses[bus] = offset
 
 print("part2:", reduce(mul, buses.keys()) - crt(buses.keys(), buses.values()))
