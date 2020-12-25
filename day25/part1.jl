69ca7c144dcac8e76de59b153fb02da75a45def3
libbies <bcw@well.com>
2020-12-25 01:40:49 -0500

    tidying up

--- day25/part1.jl
+++ day25/part1.jl
@@ -3,12 +3,8 @@ using LRUCache
 const lru = LRU{Int64, Int64}(maxsize=1)
 function recurse(iterations::Int64)
     get!(lru, iterations) do
-        uncached_recurse(iterations)
+        return 7 * recurse(iterations - 1) % 20201227
     end
-end
-
-function uncached_recurse(iterations::Int64)
-    return 7 * recurse(iterations - 1) % 20201227
 end
 
 function transform(sn::Int64, iterations::Int64)
@@ -21,20 +17,18 @@ end
 
 card_pub_key, door_pub_key = map(v -> parse(Int, v), readlines("input.txt"))
 
-loop = 1
-setindex!(lru, 7, 1)
+loop = 0
+setindex!(lru, 1, 0)
 while true
-    global loop
+    global loop += 1
     key = recurse(loop)
     if key == card_pub_key
         println("card loop: ", loop)
         println("part1: ", transform(door_pub_key, loop))
         break
-    end
-    if key == door_pub_key
+    elseif key == door_pub_key
         println("door loop: ", loop)
         println("part1: ", transform(card_pub_key, loop))
         break
     end
-    loop += 1
 end
